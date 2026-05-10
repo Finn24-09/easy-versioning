f5921ee69e810593b1421c4c8bcebcbe5378d2c1
Finn <mail@finn-marks.de>
2026-05-10 23:41:14 +0200

    chore: scaffold TypeScript action project

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  clearMocks: true,
+  verbose: false
+};
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import * as core from '@actions/core';
+
+export async function run(): Promise<void> {
+  core.info('easy-versioning: not yet implemented');
+}
